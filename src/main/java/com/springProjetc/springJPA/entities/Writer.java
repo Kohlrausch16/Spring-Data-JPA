d9dbb3f645878c3440e5b6360deffc99ecc9e3c5
João Herique Kohlrausch <joaohenriquekohlrausch@gmail.com>
2025-01-28 00:50:24 -0300

    criacao entitdade Writer

---
+++ src/main/java/com/springProjetc/springJPA/entities/Writer.java
@@ -0,0 +1,38 @@
+package com.springProjetc.springJPA.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "writer")
+@Getter
+@Setter
+public class Writer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, unique = true)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Date birthdate;
+
+    @Column(nullable = false)
+    private String nationality;
+
+    @Deprecated
+    public Writer(){
+
+    }
+
+    public Writer(String name, Date birthdate, String nationality) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.nationality = nationality;
+    }
+}
